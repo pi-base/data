a89a7ca5d7cb22902a28a38834d8c079bc24e2ba
Steven Clontz <steven.clontz@gmail.com>
2024-06-14 18:39:41 +0000

    🧼

--- πBase/Theorems/T226/Proof.lean
+++ πBase/Theorems/T226/Proof.lean
@@ -3,12 +3,9 @@ import πBase.Properties.P99.Def
 
 open Filter Topology
 
-theorem T226 : P99 ≤ P2 := fun X _ ⟨p99⟩ ↦ by
-  suffices ∀ y, IsClosed ({y} : Set X) by exact { t1 := this }
-  apply fun y ↦ isClosed_of_closure_subset ?_
-  apply fun x xincly ↦ Set.mem_singleton_iff.mp <| p99 x y (fun _ ↦ y) (fun U Unhd ↦ ?_)
-    tendsto_const_nhds
-  apply mem_map.mpr <| mem_atTop_sets.mpr ?_
-  simp only [ge_iff_le, Set.mem_preimage]
-  exact ⟨0,
-    fun _ _ ↦ Set.inter_singleton_nonempty.mp <| (mem_closure_iff_nhds.mp xincly) U Unhd⟩
+theorem T226 : P99 ≤ P2 := fun _ _ ⟨p99⟩ ↦ by
+  suffices ∀ y, IsClosed {y} by exact { t1 := this }
+  exact fun y ↦ isClosed_of_closure_subset fun x xincly ↦ Set.mem_singleton_iff.mp <|
+    p99 x y (fun _ ↦ y) (fun U Unhd ↦ mem_map.mpr <| mem_atTop_sets.mpr
+    ⟨0, fun _ _ ↦ Set.mem_preimage.mpr <| Set.inter_singleton_nonempty.mp <|
+    mem_closure_iff_nhds.mp xincly U Unhd⟩) tendsto_const_nhds
